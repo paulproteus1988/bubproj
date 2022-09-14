0116b36ba7567cbd7cb221dafeb3cea977df02ea
KilgoreTrout <sdunn133@gmail.com>
2022-09-13 21:35:31 -0400

    Add ability to write transaction to file

--- main.go
+++ main.go
@@ -3,13 +3,45 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"strconv"
 	"strings"
+	"time"
 )
+
+var TRANSACTION_PATH string = "./files/transactionlog.txt"
 
 type Transaction struct {
 	date, name, category, account string
 	amount                        float64
+}
+
+// Receive pointer to Transaction struct, prompt user for input for Transaction fields
+func addTransaction(t *Transaction) string {
+	fmt.Print("Date: ")
+	// Validate date, returns empty string if invalid
+	date, err := time.Parse("01-02-2006", userInput())
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	t.date = date.Format("01-02-2006")
+	fmt.Print("Name: ")
+	t.name = strings.TrimSpace(userInput())
+	fmt.Print("Account: ")
+	t.category = strings.TrimSpace(userInput())
+	fmt.Print("Category: ")
+	t.account = strings.TrimSpace(userInput())
+	fmt.Print("Amount: ")
+	// Validate float, returns empty string if invalid
+	amount, err2 := strconv.ParseFloat(userInput(), 64)
+	if err2 != nil {
+		fmt.Println("Error: Must be a valid value")
+		return ""
+	}
+	t.amount = amount
+	return fmt.Sprintf("%v %v \n\t%-39v %.2f\n\t%-40v %.2f", t.date, t.name, t.account, -t.amount, t.category, t.amount)
 }
 
 // Receives standard user input, terminates at newline, and returns whitespace trimmed input string
@@ -22,12 +54,33 @@ func userInput() string {
 	return strings.TrimSpace(input)
 }
 
+// Writes string to file
+func writeFile(filePath string, input string) {
+	f, err := os.Create(filePath)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	data := []byte(input)
+
+	_, err2 := f.Write(data)
+
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+}
+
 // Main menu - Prompts user for keyboard input and triggers chosen function
 func mainMenu() {
 	fmt.Print("Enter command (Enter \"help\" for help): ")
 	switch input := userInput(); input {
 	case "add":
-		addTransMenu()
+		transaction := Transaction{}
+		transText := addTransaction(&transaction)
+		writeFile(TRANSACTION_PATH, transText)
 	case "areg":
 		fmt.Println("Show transactions in particular account")
 	case "accounts":
@@ -40,13 +93,6 @@ func mainMenu() {
 		fmt.Println("Unknown command")
 		mainMenu()
 	}
-}
-
-func addTransMenu() {
-	fmt.Println("Enter transaction date: ")
-	fmt.Println("Enter name for transaction: ")
-	fmt.Println("Enter transaction catagory: ")
-	fmt.Println("Enter payment account: ")
 }
 
 func main() {
